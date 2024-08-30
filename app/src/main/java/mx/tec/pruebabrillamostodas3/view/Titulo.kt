643411fbdf369c5b81468eff76e6eb16dbf8f831
SantiagoChevez <a01749887@tec.mx>
2024-08-30 09:33:22 -0600

    El titulo acepta el color del texto

--- app/src/main/java/mx/tec/pruebabrillamostodas3/view/Titulo.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/view/Titulo.kt
@@ -6,16 +6,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Titulo(titulo: String, modifier: Modifier = Modifier){
+fun Titulo(titulo: String, modifier: Modifier = Modifier, color: Color = MaterialTheme.colorScheme.primary){
 
         Text(
             text = titulo,
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.titleLarge,
+            color = color,
             fontSize = 48.sp,
             modifier = modifier.fillMaxWidth()
         )
