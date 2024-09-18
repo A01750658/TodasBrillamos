b328e0f4e82b5e0a05f4f992620b1c89fb451aa8
Alan Vega <a01750658@tec.mx>
2024-09-18 15:55:34 -0600

    Pagos y cambios a cosas que no recuerdo qué eran xD

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     //kotlin("plugin.serialization") version "2.0.20"
 }
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2") // or your current version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0") // or your current version
+    }
+}
