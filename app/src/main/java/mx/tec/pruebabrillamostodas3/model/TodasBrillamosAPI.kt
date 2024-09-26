7ad2d20b08740b44d42ae5fc7b27509e08fb8597
Iker Fuentes <A01749675@tec.mx>
2024-09-26 11:59:06 -0600

    Documentation

--- app/src/main/java/mx/tec/pruebabrillamostodas3/model/TodasBrillamosAPI.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/model/TodasBrillamosAPI.kt
@@ -9,6 +9,14 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
+/**
+ * @author Carlos Iker Fuentes Reyes
+ * Clase abstracta para la funcionalidad de retrofit,
+ * que define los endpoints de la API
+ * y los métodos para acceder a ellos.
+ */
+
+
 const val addUserEndpoint = "add/user"
 const val addOrderwithTokenEndpoint = "add/order"
 const val getJWTKeyEndpoint = "getToken/"
@@ -22,62 +30,116 @@ const val getOrderInfoWithTokenEndpoint = "get_order/"
 interface TodasBrillamosAPI
 {
 
+
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+    Función que descarga los productos de la base de datos
+     @param user_token: Token del usuario
+     @return [ListaProducto]: Lista de productos
+     */
     @GET(getProductsWithTokenEndpoint)
     suspend fun getProductos( @Query("user_token") user_token : String): ListaProducto
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+    Función que descarga la imagen de un producto
+     @param image_id: Id de la imagen
+     @return [ResponseBody]: Imagen. Response body es un formato estandar de respuesta del JSON
+     */
     @GET(getProductImageEndpoint)
     suspend fun getProductImage(@Query("image_id") image_id : String): ResponseBody
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que agrega un usuario a la base de datos
+     * @param user: Usuario a agregar
+     * @return [ResponseFormat]: Respuesta del servidor.
+     */
     @POST(addUserEndpoint)
     suspend fun addUser(@Body user: Usuario): ResponseFormat
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que obtiene el token de un usuario
+     * @param in_email [String]]: Email del usuario
+     * @param in_password [String]: Contraseña del usuario
+     * @return [JWT_KEY]: Token del usuario.
+     */
+
     @GET(getJWTKeyEndpoint)
     suspend fun getJWTKey(@Query("in_email") in_email : String, @Query("in_password") in_password : String): JWT_KEY
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que agrega una orden a la base de datos
+     * @param order [Order]: Orden a agregar
+     * @param user_token [String] token del usuario
+     * @return [ResponseFormat]: Respuesta del servidor.
+     */
     @POST(addOrderwithTokenEndpoint)
     suspend fun addOrderWithToken(@Body order: Order, @Query("user_token") user_token : String): ResponseFormat
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que agrega una dirección a la base de datos
+     * @param address [Direccion]: Dirección a agregar
+     * @param user_token [String]: Token del usuario
+     */
     @POST(addAddressWithTokenEndpoint)
     suspend fun addAddressWithToken(@Body address: Direccion, @Query("user_token") user_token : String): ResponseFormat
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que obtiene los datos de un usuario
+     * @param user_token [String]: Token del usuario
+     * @return [DataUsuario]: Datos del usuario.
+     */
     @GET(getUserDataWithTokenEndpoint)
     suspend fun getUserDataWithToken(@Query("user_token") user_token : String) : DataUsuario
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que obtiene el token de recuperación de contraseña
+     * @param in_correo_cliente [String]: Correo del usuario
+     * @return [ResponseFormat]: Respuesta del servidor.
+     */
     @GET(getRecoveryPasswordTokenEndpoint)
     suspend fun getRecoveryPasswordToken(@Query("in_correo_cliente") in_correo_cliente : String): ResponseFormat
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que hace la solicitud de cambiar la contraseña
+     * @param change [ChangePassword]: Contraseña a cambiar
+     * @return [ResponseFormat]: Respuesta del servidor.
+     */
     @POST(changePasswordEndpoint)
     suspend fun changePassword(@Body change: ChangePassword ): ResponseFormat
 
     @Headers(
         "User-Agent:Retrofit"
     )
+    /**
+     * Función que obtiene la información de una orden
+     * @param user_token [String]: Token del usuario
+     * @return [Orders]: Información de la orden.
+     */
     @GET(getOrderInfoWithTokenEndpoint)
     suspend fun getOrderInfoWithToken(@Query("user_token") user_token : String): Orders
 }
