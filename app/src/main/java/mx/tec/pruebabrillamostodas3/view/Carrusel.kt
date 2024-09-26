795563d9e41c6c58386e2f72b5a0ed026e9ef773
SantiagoChevez <a01749887@tec.mx>
2024-09-26 13:28:36 -0600

    Carrusel y colores pantalla de información

--- app/src/main/java/mx/tec/pruebabrillamostodas3/view/Carrusel.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/view/Carrusel.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -24,21 +26,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun Carrusel(titulos: List<String>, lista: List<String>, modifier: Modifier = Modifier){
+fun Carrusel(titulos: List<String>, lista: List<String>, colors: List<Color>, modifier: Modifier = Modifier){
     var currentImageIndex by remember { mutableStateOf(0) }
-    var colors = listOf(MaterialTheme.colorScheme.tertiary,MaterialTheme.colorScheme.primaryContainer ,MaterialTheme.colorScheme.secondaryContainer)
     Box(modifier = modifier) {
             ElevatedCard(
                 modifier = Modifier
                     .padding(8.dp),
                 colors = CardDefaults.cardColors(
-                    containerColor = colors[currentImageIndex%3],
+                    containerColor = colors[currentImageIndex%colors.size],
                     contentColor = MaterialTheme.colorScheme.onTertiary
                 )
             ) {
@@ -74,13 +76,20 @@ fun Carrusel(titulos: List<String>, lista: List<String>, modifier: Modifier = Mo
             ) {
                 Button(onClick = {
                     currentImageIndex = (currentImageIndex - 1 + lista.size) % lista.size
-                }, modifier = Modifier.padding(end = 8.dp)) {
+                }, modifier = Modifier.padding(bottom = 8.dp, start = 8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colors[(currentImageIndex + 1)% colors.size],
+                        contentColor = MaterialTheme.colorScheme.onTertiary)
+                ) {
                     Icon(Icons.Default.ArrowBack, contentDescription = "Anterior", tint = MaterialTheme.colorScheme.onTertiary)
                 }
 
                 Button(onClick = {
                     currentImageIndex = (currentImageIndex + 1) % lista.size
-                }) {
+                }, modifier = Modifier.padding(bottom = 8.dp, end = 8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                    containerColor = colors[(currentImageIndex +1)%colors.size],
+                    contentColor = MaterialTheme.colorScheme.onTertiary)) {
                     Icon(Icons.Default.ArrowForward, contentDescription = "Siguiente", tint = MaterialTheme.colorScheme.onTertiary)
                 }
             }
