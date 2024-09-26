3caf8b90ba059cb460052ddd5a37c7e1d7540700
Iker Fuentes <A01749675@tec.mx>
2024-09-26 12:58:27 -0600

    Documentación

--- app/src/main/java/mx/tec/pruebabrillamostodas3/model/Order.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/model/Order.kt
@@ -1,7 +1,17 @@
 package mx.tec.pruebabrillamostodas3.model
 
 import kotlinx.serialization.Serializable
+/**
+ * @author Carlos Iker Fuentes Reyes
+ * Clase de datos que se utiliza como formato para la
+ * lectura del JSON en la clase abstracta Todasbrillamos API
 
+Formato JSON
+{
+    "data_info":"17,18;4,5",
+    "id_usuario":-1
+}
+ */
 @Serializable
 data class Order(
     val data_info : String,
--- app/src/main/java/mx/tec/pruebabrillamostodas3/model/ResponseFormat.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/model/ResponseFormat.kt
@@ -1,5 +1,15 @@
 package mx.tec.pruebabrillamostodas3.model
+/**
+ * @author Carlos Iker Fuentes Reyes
+ * Clase de datos que se utiliza como formato para la
+ * lectura del JSON en la clase abstracta Todasbrillamos API
 
+ * Formato del JSON
+{
+    "result": "unsuccessful",
+    "message": "Error"
+}
+ */
 data class ResponseFormat(
     val result :String,
     val message : String
