66c5781bf7ec28058e786640f3701c73c3681c05
SantiagoChevez <a01749887@tec.mx>
2024-10-01 15:57:05 -0600

    Cambios a la barra de busqueda

--- app/src/main/java/mx/tec/pruebabrillamostodas3/view/Foros.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/view/Foros.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,142 +47,148 @@ import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
-fun Foros(){
+fun Foros() {
     var showBottomSheet by remember { mutableStateOf(false) }
     var showBottomSheet2 by remember { mutableStateOf(false) }
-            Box(
+    var searchText by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.secondary),
+    )
+    {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+
+        ) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.secondaryContainer,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.secondary),
+                    .padding(10.dp)
+                    .size(100.dp)
+                    .fillMaxWidth(),
+
+                )
+            Titulo(
+                titulo = "Preguntas Frecuentas",
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                fontSize = 50,
+                lineHeight = 40
             )
-            {
-                if (showBottomSheet) {
-                    CrearForo{
-                        showBottomSheet = false
-                    }
-                } else if (showBottomSheet2) {
-                    TempleteForo("Acaso ¿Esta es la pregunta más interesante sobre el tema?", listOf("Súper si", "Nah", "Tal vez", "Como crees!"))
-                } else{
-                    Column( horizontalAlignment =  Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .fillMaxWidth()
+            Spacer(
+                modifier = Modifier
+                    .padding(6.dp)
+                    .fillMaxWidth()
+            )
+            HorizontalDivider(
+                thickness = 2.dp,
+                color = MaterialTheme.colorScheme.primaryContainer
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            )
+            Etiqueta(
+                "Buscar",
+                Modifier.padding(bottom = 3.dp),
+                color = Color.Black,
+                padding = 30
+            )
+            //Checar si se puede hacer con searchbar
+            InputPregunta(text =searchText, onValueChange ={ nuevoTexto -> searchText = nuevoTexto} )
+            /*Row {
+                InputTexto("", {}, modifier = Modifier.weight(5f))
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Busqueda",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(onClick = { /*TODO*/ })
+                        .size(50.dp)
+                        .weight(1f)
+                )
+            }*/
+            HorizontalDivider(
+                color = MaterialTheme.colorScheme.primary,
+                thickness = 2.dp
+            )
 
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Star,
-                            contentDescription = "",
-                            tint = MaterialTheme.colorScheme.secondaryContainer,
-                            modifier = Modifier
-                                .padding(10.dp)
-                                .size(100.dp)
-                                .fillMaxWidth(),
-
-                            )
-                        Titulo(titulo = "Foros", color = MaterialTheme.colorScheme.secondaryContainer, fontSize = 50)
-                        Spacer(modifier = Modifier
-                            .padding(6.dp)
-                            .fillMaxWidth()
-                        )
-                        HorizontalDivider(
-                            thickness = 2.dp,
-                            color = MaterialTheme.colorScheme.primaryContainer
-                        )
-                        Spacer(
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth()
-                        )
-                        Etiqueta(
-                            "Buscar",
-                            Modifier.padding(bottom = 3.dp),
-                            color = Color.Black,
-                            padding = 30
-                        )
-                        //Checar si se puede hacer con searchbar
-                        Row {
-                            InputTexto("", {}, modifier = Modifier.weight(5f))
-                            Icon(
-                                imageVector = Icons.Default.Search,
-                                contentDescription = "Busqueda",
+            Box {
+                LazyColumn{
+                    for (i in 1..3) {
+                        item {
+                            ElevatedCard(
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable(onClick = { /*TODO*/ })
-                                    .size(50.dp)
-                                    .weight(1f)
-                            )
-                        }
-                        HorizontalDivider(
-                            color = MaterialTheme.colorScheme.primary,
-                            thickness = 2.dp
-                        )
-
-                        Box {
-                            LazyColumn() {
-                                for (i in 1..5) {
-                                    item {
-                                        ElevatedCard(
-                                            modifier = Modifier
-                                                .padding(8.dp)
-                                                .clickable(onClick = { showBottomSheet2 = true })
-                                        ) {
-                                            Text(
-                                                text = "Foro 1.$i",
-                                                textAlign = TextAlign.Center,
-                                                modifier = Modifier
-                                                    .fillMaxWidth()
-                                                    .padding(24.dp)
-                                            )
-                                        }
-                                    }
-                                    item {
-                                        ElevatedCard(
-                                            modifier = Modifier
-                                                .padding(8.dp)
-                                                .clickable(onClick = { showBottomSheet2 = true })
-                                        ) {
-                                            Text(
-                                                text = "Foro 2.$i",
-                                                textAlign = TextAlign.Center,
-                                                modifier = Modifier
-                                                    .fillMaxWidth()
-                                                    .padding(24.dp)
-                                            )
-                                        }
-                                    }
-                                    item {
-                                        ElevatedCard(
-                                            modifier = Modifier
-                                                .padding(8.dp)
-                                                .clickable(onClick = { showBottomSheet2 = true })
-                                        ) {
-                                            Text(
-                                                text = "Foro 3.$i",
-                                                textAlign = TextAlign.Center,
-                                                modifier = Modifier
-                                                    .fillMaxWidth()
-                                                    .padding(24.dp)
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                            FloatingActionButton(
-                                onClick = { showBottomSheet= true },
-                                containerColor = MaterialTheme.colorScheme.tertiary,
-                                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
-                                modifier = Modifier
-                                    .align(Alignment.BottomEnd)
-                                    .padding(bottom = 16.dp),
+                                    .padding(8.dp)
+                                    .clickable(onClick = { showBottomSheet2 = true })
                             ) {
-                                Icon(
-                                    imageVector = Icons.Default.Create,
-                                    contentDescription = "Generar"
+                                Text(
+                                    text = "Foro 1.$i",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(24.dp)
                                 )
-
+                            }
+                        }
+                        item {
+                            ElevatedCard(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .clickable(onClick = { showBottomSheet2 = true })
+                            ) {
+                                Text(
+                                    text = "Foro 2.$i",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(24.dp)
+                                )
+                            }
+                        }
+                        item {
+                            ElevatedCard(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .clickable(onClick = { showBottomSheet2 = true })
+                            ) {
+                                Text(
+                                    text = "Foro 3.$i",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(24.dp)
+                                )
                             }
                         }
                     }
                 }
+                FloatingActionButton(
+                    onClick = { showBottomSheet = true },
+                    containerColor = MaterialTheme.colorScheme.tertiary,
+                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(bottom = 16.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Create,
+                        contentDescription = "Generar"
+                    )
+
+                }
             }
         }
+    }
+}
+
+fun onSearch(it: String) {
+    println("Buscar")
+}
