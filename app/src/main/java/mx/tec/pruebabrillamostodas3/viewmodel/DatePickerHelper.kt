c1ff791e137c8c75f61c135b9151d6a7a14cdf4a
Cesar Flores <A01751101@tec.mx>
2024-10-02 14:30:25 -0600

    date picker ya muestra seleccion

--- app/src/main/java/mx/tec/pruebabrillamostodas3/viewmodel/DatePickerHelper.kt
+++ app/src/main/java/mx/tec/pruebabrillamostodas3/viewmodel/DatePickerHelper.kt
@@ -8,17 +8,17 @@ import mx.tec.pruebabrillamostodas3.viewmodel.BTVM
 
 class DatePickerHelper (private val context: Context) {
 
-    fun initializeDatePicker(datePicker: DatePicker): Triple<Int, Int, Int> {
+    fun initializeDatePicker(datePicker: DatePicker, btVM: BTVM) {
         val today = Calendar.getInstance()
-        var selectedDate = Triple(today.get(Calendar.YEAR), today.get(Calendar.MONTH) + 1, today.get(Calendar.DAY_OF_MONTH))
+        var date = Triple(0,0,0)
+        //var selectedDate = Triple(today.get(Calendar.YEAR), today.get(Calendar.MONTH) + 1, today.get(Calendar.DAY_OF_MONTH))
         datePicker.init(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH)){
                 _, year, month, day ->
             val month = month + 1
             //val msg = "You selected: $day/$month/$year"
-            selectedDate = Triple(year, month, day)
+            btVM.setFecha(day, month, year)
             //Aquí abajo se maneja la fecha, ahora mismo muestra una alerta con el Toast
-            //Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, selectedDate.toString(), Toast.LENGTH_SHORT).show()
         }
-        return selectedDate
     }
 }
