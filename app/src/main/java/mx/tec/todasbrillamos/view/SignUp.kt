c3f8c8261177c9880c58dda0544c34d1c5137de2
Cesar Flores <A01751101@tec.mx>
2024-10-14 11:31:43 -0600

    hash password signup->login, fix 3

--- app/src/main/java/mx/tec/todasbrillamos/view/SignUp.kt
+++ app/src/main/java/mx/tec/todasbrillamos/view/SignUp.kt
@@ -401,9 +401,19 @@ fun SignUp(btVM: BTVM, navController: NavHostController) {
 
             LaunchedEffect(estadoErrors.value.errorSignUp) {
                 if (!estadoErrors.value.errorSignUp) {
-                    navController.navigate(Pantallas.RUTA_LOGIN)
+                    //navController.navigate(Pantallas.RUTA_LOGIN)
                     btVM.saveHashPassword(context, valorPassword)
-                    btVM.setErrorSignUp(true)
+                    //btVM.setErrorSignUp(true)
+                }
+            }
+
+            LaunchedEffect(btVM.hashGuardado.value) {
+                if (btVM.hashGuardado.value == true) {
+                    navController.navigate(Pantallas.RUTA_LOGIN)
+                    //btVM.saveHashPassword(context, valorPassword)
+                    //btVM.setErrorSignUp(true)
+                    btVM.setErrorLogin(false)
+                    btVM.setHashGuardado(false)
                 }
             }
 
